37f62e04e58491443ecba544ff5803165f044201
mayne <i@@gine.me>
2023-04-26 05:20:22 +0800

    fix: api cors

--- src/pages/api/openai/prompt.ts
+++ src/pages/api/openai/prompt.ts
@@ -1,13 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { type NextApiRequest, type NextApiResponse } from "next";
+import Cors from "cors";
 
 import { mockTable } from "~/server/openai/prompt";
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
 
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 // get data from body
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Run the middleware
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  await runMiddleware(req, res, cors);
   if (req.method !== "POST") {
     res.status(405).send({ message: "Only POST requests allowed" });
     return;
